035dbef19948b7602ae1cae30d99952739d11913
Juliano Lombardi <juliano.lombardi@infinityworks.com>
2022-06-19 22:36:41 +0100

    added js-refresher and node practice files

--- js-refresher.js
+++ js-refresher.js
@@ -1,26 +1,26 @@
-const person = {
-  name: "Max",
-  age: 29,
-  greet() {
-    console.log("Hi, I am " + this.name);
-  },
-};
+// const person = {
+//   name: "Max",
+//   age: 29,
+//   greet() {
+//     console.log("Hi, I am " + this.name);
+//   },
+// };
 
-const printName = ({ name }) => {
-  console.log(name);
-};
+// const printName = ({ name }) => {
+//   console.log(name);
+// };
 
-printName(person);
+// printName(person);
 
-const { name, age } = person;
-console.log(name, age);
+// const { name, age } = person;
+// console.log(name, age);
 
-// const copiedPerson = { ...person };
-// console.log(copiedPerson);
+// // const copiedPerson = { ...person };
+// // console.log(copiedPerson);
 
-const hobbies = ["Sports", "Cooking"];
-const [hobby1, hobby2] = hobbies;
-console.log(hobby1, hobby2);
+// const hobbies = ["Sports", "Cooking"];
+// const [hobby1, hobby2] = hobbies;
+// console.log(hobby1, hobby2);
 
 // // for (let hobby of hobbies) {
 // //     console.log(hobby);
@@ -35,3 +35,16 @@ console.log(hobby1, hobby2);
 // };
 
 // console.log(toArray(1, 2, 3, 4));
+
+const fetchData = (callback) => {
+  setTimeout(() => {
+    callback("done");
+  }, 1500);
+};
+
+setTimeout(() => {
+  console.log("Timer is done!");
+  fetchData((text) => {
+    console.log(text);
+  });
+}, 2000);
