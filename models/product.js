6f6250a6b8e79987fa39385fd8b76f16cad34686
Juliano Lombardi <juliano.lombardi@infinityworks.com>
2022-07-31 22:57:30 +0100

    created data folder and changed products to save and read products from readFile

--- models/product.js
+++ models/product.js
@@ -1,4 +1,5 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
 
 module.exports = class Product {
   constructor(productTitle) {
@@ -6,12 +7,42 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
-    console.log("this product:", this, "saved products:", products);
+    const productsPath = path.join(
+      path.dirname(process.mainModule.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(productsPath, (error, fileContent) => {
+      console.log("fileContent:", JSON.parse(fileContent));
+      let products = [];
+      if (!error) {
+        products = JSON.parse(fileContent);
+      } else {
+        console.log(error);
+      }
+      products.push(this);
+      console.log("saved product:", this);
+      fs.writeFile(productsPath, JSON.stringify(products), (error) => {
+        if (error) {
+          console.log("error:", error);
+        }
+      });
+    });
   }
 
-  static fetchAll() {
-    console.log("fetching products:", products);
-    return products;
+  static fetchAll(callBack) {
+    const productsPath = path.join(
+      path.dirname(process.mainModule.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(productsPath, (error, fileContent) => {
+      if (error) {
+        console.log(error, "fetchAll returned an empty array");
+        callBack([]);
+      }
+      console.log("fetching products:", JSON.parse(fileContent));
+      callBack(JSON.parse(fileContent));
+    });
   }
 };
